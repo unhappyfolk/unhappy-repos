f0a2e62b267f2ccc70982bb736cf552946cfdf6d
Muhammad Hashim <msg@mhashim6.me>
2023-04-08 00:04:18 +0200

    auto-fetch repositories issues every Friday
    Signed-off-by: Muhammad Hashim <msg@mhashim6.me>

--- index.js
+++ index.js
@@ -1,65 +1,99 @@
+import { readFileSync, writeFileSync } from "fs";
 import { Octokit } from "octokit";
 
+const githubRepositories = JSON.parse(
+  readFileSync("./github_repositories.json")
+);
+
+const { GH_TOKEN = undefined } = process.env;
+
 const octokit = new Octokit({
-  // auth: 'YOUR-TOKEN'
+  auth: GH_TOKEN,
 });
-const QURAN_ORG = "quran";
-const quran_android = `quran_android`;
+
+const saveIssues = (repos) =>
+  writeFileSync("./github_repositories.json", JSON.stringify(repos), "utf-8");
+
+const range = (start, end) =>
+  Array.from({ length: end - start + 1 }, (_, i) => i);
+
+const languages = async (owner, repo) => {
+  const langs = (
+    await octokit.request(`GET /repos/${owner}/${repo}/languages`, {
+      owner,
+      repo,
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    })
+  ).data;
+  return Object.keys(langs);
+};
+
+const openIssuesCount = async (owner, repo) => {
+  return (
+    await octokit.request(`GET /repos/${owner}/${repo}`, {
+      owner,
+      repo,
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    })
+  ).data["open_issues_count"];
+};
 
 /** fetches a single page with a max of 100 issues. */
-const issues = (owner, repo, page) =>
-  octokit.request(`GET /repos/${owner}/${repo}/issues`, {
-    owner,
-    repo,
-    headers: {
-      "X-GitHub-Api-Version": "2022-11-28",
-    },
-    state: "open",
-    per_page: 100,
-    page,
-  });
+const issuesPerPage = async (owner, repo, page) =>
+  (
+    await octokit.request(`GET /repos/${owner}/${repo}/issues`, {
+      owner,
+      repo,
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+      state: "open",
+      per_page: 100,
+      page,
+    })
+  ).data.filter((it) => it.labels.length > 0);
 
-/** @deprecated no longer needed */
-const labelsInPage = async (owner, repo, page) =>
-  (await issues(owner, repo, page)).data
-    .filter((it) => it.labels.length > 0)
-    .flatMap((it) => it.labels)
-    .map((it) => it.name);
+const issues = async (owner, repo, issuesCount) => {
+  const pagesNum = Math.ceil(issuesCount / 100);
 
-const fetchLabels = (owner, repo) =>
-  octokit.request(`GET /repos/${owner}/${repo}/labels`, {
-    owner,
-    repo,
-    headers: {
-      "X-GitHub-Api-Version": "2022-11-28",
-    },
-    state: "open",
-    per_page: 100,
-  });
+  const pages = range(0, pagesNum).map((page) =>
+    issuesPerPage(owner, repo, page + 1)
+  );
+  return (await Promise.all(pages))
+    .flatMap((it) => it)
+    .map((it) => ({
+      title: it.title,
+      body: it.body,
+      html_url: it.html_url,
+    }));
+};
 
-const labels = async (owner, repo) =>
-  (await fetchLabels(owner, repo)).data.map((it) => it.name);
+const fetchReposIssues = async () =>
+  (
+    await Promise.all(
+      Object.keys(githubRepositories).map(async (it) => {
+        const [org, repo] = it.split("/");
+        const langs = await languages(org, repo);
+        const issueCount = await openIssuesCount(org, repo);
+        const repoIssues = await issues(org, repo, issueCount);
+        return { org, repo, languages: langs, issues: repoIssues };
+      })
+    )
+  ).reduce(
+    (acc, it) => ({
+      ...acc,
+      [`${it.org}/${it.repo}`]: {
+        languages: it.languages,
+        issues: it.issues,
+      },
+    }),
+    {}
+  );
 
-console.log(await labels("nuqayah", "deen-projects"));
-
-//
-
-/* quran_android labels
-[
-          'Data',
-  'dependencies', 'Improvement',
-  'In progress',  'Inquiry',
-  'Major',        'Minor',
-  'New Feature',  'PRs Welcome',
-  'Suggestions'
-]
-*/
-
-/* nuqayah labels
-[
-  
-level1: 'urgency: 3', 'priority: high'
-level2: 'urgency: 2'
-level3: 'urgency: 1'
-]
-*/
+(async () => {
+  saveIssues(await fetchReposIssues());
+})();
